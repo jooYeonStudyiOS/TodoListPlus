e89eac0135ade4f364bd421f2f8863b8c2c68251
jooYeon <conn0204@naver.com>
2023-08-24 22:51:36 +0900

    카테고리 enum은 보류하고 memo 옵셔널 추가

--- TodoListPlus/TodoListPlus/Model/TodoList.swift
+++ TodoListPlus/TodoListPlus/Model/TodoList.swift
@@ -9,19 +9,11 @@ import UIKit
 
 struct TodoList {
     
-    //카테고리 추가하고 싶을 때는 어떻게 해야할까?
-    //
-    enum Cagegory {
-        case study
-        case exercise
-        case appointment
-    }
-    
     let listNumber: Int
     var isComplited: Bool
-    var category: Cagegory
+//    var category: Cagegory
     var title: String
     var date: Date
     var time: Date
-    var memo: String
+    var memo: String?
 }
