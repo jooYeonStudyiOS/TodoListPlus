49f88c20d8501c97fd76f94b7cf1d641751c7290
jooYeon <conn0204@naver.com>
2023-08-24 22:52:23 +0900

    UserDefaults 읽기, 쓰기 함수 추가

--- TodoListPlus/TodoListPlus/Model/TodoList.swift
+++ TodoListPlus/TodoListPlus/Model/TodoList.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-struct TodoList {
+struct TodoList: Codable {
     
     let listNumber: Int
     var isComplited: Bool
@@ -16,4 +16,18 @@ struct TodoList {
     var date: Date
     var time: Date
     var memo: String?
+
+    static var name: String {
+        return String(describing: self)
+    }
+    
+    static var allTodoList: [TodoList]? {
+        guard let listData = UserDefaults.standard.value(forKey: TodoList.name) as? Data,
+              let allTodoList = try? PropertyListDecoder().decode([TodoList].self, from: listData) else { return nil }
+        return allTodoList
+    }
+    
+    func addList(_ todoList: [TodoList]) {
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(todoList), forKey: TodoList.name)
+    }
 }
