19de9ac5f14721fe824bfeb2c104c09ad7f6bb60
jooYeon <conn0204@naver.com>
2023-08-24 20:40:09 +0900

    쓰기 페이지 아울렛, 액션 추가

--- TodoListPlus/TodoListPlus/Controller/WriteViewController.swift
+++ TodoListPlus/TodoListPlus/Controller/WriteViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class WriteViewController: UIViewController {
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var dateTimeSeleteButton: UIButton!
+    @IBOutlet weak var memoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func didTappedDateTimeSeleteButton(_ sender: Any) {
+    }
+    
+    @IBAction func didTappedDoneButton(_ sender: Any) {
     }
 }
